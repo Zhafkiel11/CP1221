ff1e336f336f8dbe1d4df44d89e94d31e5fb8b66
Zhaf <119938891+Zhafkiel11@users.noreply.github.com>
2024-03-13 13:37:40 +0800

    Add files via upload
    VB Files

---
+++ EsguerraVB/EsguerraVB/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim username As String
+        Dim Password As String
+
+        username = TextBox1.Text
+        Password = TextBox2.Text
+
+        MsgBox("Username: " + username + vbCrLf +
+               "Password: " + Password)
+
+    End Sub
+End Class
